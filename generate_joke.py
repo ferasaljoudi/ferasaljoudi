a9218f6bb717503de529479751c142a2cd8cf5c4
Feras Aljoudi <114940180+ferasaljoudi@users.noreply.github.com>
2024-05-25 01:31:13 -0600

    testing

--- generate_joke.py
+++ generate_joke.py
@@ -1,16 +1,8 @@
 import requests
-from bs4 import BeautifulSoup
-import subprocess
 
 joke_url = "https://readme-jokes.vercel.app/api?hideBorder&qColor=%238C52FF&aColor=%23EFE372&bgColor=black"
 response = requests.get(joke_url)
 joke_svg = response.text
 
-soup = BeautifulSoup(joke_svg, "html.parser")
-for script in soup(["script", "style"]):
-    script.decompose()
-
-cleaned_joke_svg = str(soup)
-
 with open("randomJoke.svg", "w") as file:
-    file.write(cleaned_joke_svg)
+    file.write(joke_svg)
