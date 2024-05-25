f7867c84f3163a37ad948f2d96c020ac96172215
Feras Aljoudi <114940180+ferasaljoudi@users.noreply.github.com>
2024-05-25 00:49:22 -0600

    testing the readme joke

--- generate_joke.py
+++ generate_joke.py
@@ -2,20 +2,20 @@ import requests
 
 joke_url = "https://readme-jokes.vercel.app/api?hideBorder&qColor=%238C52FF&aColor=%23EFE372&bgColor=black"
 response = requests.get(joke_url)
-joke_svg = response.text
+joke_svg_url = response.url
 
 with open("README.md", "r") as file:
     readme = file.readlines()
 
 marker = "<!-- JOKE HERE -->"
 
-def insert_joke_at_marker(marker, joke_svg, readme):
+def insert_joke_at_marker(marker, joke_svg_url, readme):
     for idx, line in enumerate(readme):
         if marker in line:
-            readme[idx] = f"{marker}\n\n{joke_svg}\n"
+            readme[idx] = f"{marker}\n\n![Joke]({joke_svg_url})\n"
             break
 
-insert_joke_at_marker(marker, joke_svg, readme)
+insert_joke_at_marker(marker, joke_svg_url, readme)
 
 with open("README.md", "w") as file:
     file.writelines(readme)
