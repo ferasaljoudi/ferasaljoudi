8bd22b2de1696a1affaa7d99edf683be97f6b2b2
Feras Aljoudi <114940180+ferasaljoudi@users.noreply.github.com>
2024-05-25 10:17:02 -0600

    update link

--- generate_joke.py
+++ generate_joke.py
@@ -1,7 +1,8 @@
 import requests
 import re
 
-joke_url = "https://readme-jokes.vercel.app/api?hideBorder&qColor=%238C52FF&aColor=%23EFE372&bgColor=black"
+# joke_url = "https://readme-jokes.vercel.app/api?hideBorder&qColor=%238C52FF&aColor=%23EFE372&bgColor=black"
+joke_url = "https://readme-jokes.vercel.app/api"
 response = requests.get(joke_url)
 joke_svg = response.text
 
