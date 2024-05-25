f56e7c66b1a5c19fa73634365603abf7ef0d192c
Feras Aljoudi <114940180+ferasaljoudi@users.noreply.github.com>
2024-05-25 02:01:27 -0600

    test

--- generate_joke.py
+++ generate_joke.py
@@ -1,21 +1,28 @@
 import requests
+from bs4 import BeautifulSoup
 
 joke_url = "https://readme-jokes.vercel.app/api?hideBorder&qColor=%238C52FF&aColor=%23EFE372&bgColor=black"
 response = requests.get(joke_url)
 joke_svg = response.text
+
+soup = BeautifulSoup(joke_svg, "html.parser")
+for script in soup(["script", "style"]):
+    script.decompose()
+
+cleaned_joke_svg = str(soup)
 
 with open("README.md", "r") as file:
     readme = file.readlines()
 
 marker = "<!-- JOKE HERE -->"
 
-def insert_joke_at_marker(marker, joke_svg, readme):
+def insert_joke_at_marker(marker, cleaned_joke_svg, readme):
     for idx, line in enumerate(readme):
         if marker in line:
-            readme[idx] = f"{marker}\n\n{joke_svg}\n"
+            readme[idx] = f"{marker}\n\n{cleaned_joke_svg}\n"
             break
 
-insert_joke_at_marker(marker, joke_svg, readme)
+insert_joke_at_marker(marker, cleaned_joke_svg, readme)
 
 with open("README.md", "w") as file:
     file.writelines(readme)
